d4092a7c0f46e0a81cb7cdface6dd721a107014d
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 16:57:38 +0300

    csharp/demo.cs: add file

---
+++ csharp/src/demo.cs
@@ -0,0 +1,78 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2019 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+using System;
+
+using Google.Protobuf;
+using DocoptNet;
+
+using ZeroMQ.lib;
+using ZeroMQ;
+
+namespace Demo {
+
+internal class Program {
+    private const string usage =
+        @"Usage:
+    demo [-hDVq] [-r <addr>] [-t <time>] [URI ...]
+
+Options:
+    -h --help                       Print this help and exit
+    -D --print-config               Print configuration values and exit
+    -V --version-zmq                Print ZeroMQ version and exit
+    -r --router-endpoint <addr>     Specify the router endpoint address
+                                     [default: tcp://localhost:5570]
+    -t --connect-timeout <time>     Specify maximum time in seconds for
+                                     the connection allowed to take
+                                     [default: 30]
+    -q --be-terse                   Be brief and to the point; dump
+                                     interesting details only
+
+";
+    private static void Main(string[] args) {
+        var opts = new Docopt().Apply(usage, args, exit: true);
+
+        if (opts["--print-config"].IsTrue) {
+            Console.WriteLine("---");
+            foreach (var argument in opts) {
+                Console.WriteLine("{0}: {1}", argument.Key.Replace("--", ""),
+                                  argument.Value);
+            }
+            System.Environment.Exit(1);
+        } else if (opts["--version-zmq"].IsTrue) {
+            int major, minor, patch;
+            zmq.version(out major, out minor, out patch);
+            Console.WriteLine("ZeroMQ version {0}.{1}.{2}",
+                              major, minor, patch);
+            System.Environment.Exit(1);
+        }
+
+        var inputUri = opts["URI"].AsList;
+
+        log4net.Config.XmlConfigurator.Configure();
+        var log = log4net.LogManager.GetLogger("demo");
+
+        if (inputUri.Count == 0) {
+            log.Error("no input URI given");
+            System.Environment.Exit(1);
+        }
+
+        var jomiel = new Jomiel(opts);
+        jomiel.connect();
+
+        foreach (var uri in inputUri) {
+            jomiel.inquire(uri.ToString());
+        }
+    }
+}
+
+} // end of namespace
