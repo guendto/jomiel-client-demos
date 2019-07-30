3da6ba1ad31c27e7f2e815f3d70924f812a7afc4
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 17:19:42 +0300

    rust/main.rs: add file

---
+++ rust/src/main.rs
@@ -0,0 +1,59 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2019 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+extern crate protobuf;
+extern crate structopt;
+extern crate zmq;
+
+/*
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
+*/
+
+use jomiel::Jomiel;
+
+mod jomiel;
+mod options;
+mod proto;
+
+fn main() {
+    let jomiel = Jomiel::new();
+
+    if jomiel.opts.print_config {
+        print_config(&jomiel);
+    } else if jomiel.opts.version_zmq {
+        print_zmq_version()
+    }
+
+    if jomiel.opts.uri.len() < 1 {
+        println!("error: input URI not given");
+        std::process::exit(1);
+    }
+
+    jomiel.connect();
+
+    for uri in &jomiel.opts.uri {
+        jomiel.inquire(uri);
+    }
+}
+
+fn print_config(jomiel: &Jomiel) {
+    println!("{:#?}", jomiel.opts);
+    std::process::exit(0);
+}
+
+fn print_zmq_version() {
+    let (major, minor, patch) = zmq::version();
+    println!("ZeroMQ version {}.{}.{}", major, minor, patch);
+    std::process::exit(0);
+}
