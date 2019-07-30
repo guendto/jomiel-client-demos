693beaa061729aeac05f6787f9104382fa0ffa71
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2019-07-30 16:59:23 +0300

    golang/options.go: add file

---
+++ golang/src/jomiel.github.io/demo/examples/options.go
@@ -0,0 +1,67 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2019 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package demo
+
+import flag "github.com/spf13/pflag"
+
+type Options struct {
+	RouterEndpoint string `json:"router-endpoint"`
+	ConnectTimeout int    `json:"connect-timeout"`
+	PrintConfig    bool
+	VersionZmq     bool
+	OutputJson     bool `json:"output-json"`
+	BeTerse        bool `json:"be-terse"`
+}
+
+func NewOptions() *Options {
+	opts := &Options{}
+
+	flag.BoolVarP(
+		&opts.PrintConfig,
+		"print-config", "D",
+		false,
+		"Print configuration values and exit")
+
+	flag.StringVarP(
+		&opts.RouterEndpoint,
+		"router-endpoint", "r",
+		"tcp://localhost:5570",
+		"Specify the router endpoint address")
+
+	flag.IntVarP(
+		&opts.ConnectTimeout,
+		"connect-timeout", "t",
+		30,
+		"Specify maximum time in seconds for the connection allowed to take")
+
+	flag.BoolVarP(
+		&opts.VersionZmq,
+		"version-zmq",
+		"V",
+		false,
+		"Display ZeroMQ version and exit")
+
+	flag.BoolVarP(
+		&opts.OutputJson,
+		"output-json", "j",
+		false,
+		"Print dumped messages in JSON")
+
+	flag.BoolVarP(
+		&opts.BeTerse,
+		"be-terse", "q",
+		false,
+		"Be brief and to the point; dump interesting details only")
+
+	return opts
+}
