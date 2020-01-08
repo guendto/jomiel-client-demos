0fb9c35215c7e0e06cea97a0875c51767aa05d13
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2020-01-08 15:22:12 +0200

    qt/main.cpp: add file

---
+++ qt/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * Copyright
+ *  2020 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "DemoAppWindow"
+
+int main(int argc, char** argv) {
+  return demo::DemoAppWindow::main(argc, argv);
+}
+
+// vim: set ts=2 sw=2 tw=72 expandtab:
