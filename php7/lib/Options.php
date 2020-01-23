79c494c5796e763a66cb71f40b9b8248d71ef2f9
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2020-01-23 19:05:36 +0200

    php7/Options: apply prettier changes

--- php7/lib/Options.php
+++ php7/lib/Options.php
@@ -18,48 +18,58 @@ use GetOptionKit\OptionParser;
 
 use \ZMQ;
 
-function opts_init($argv) {
-    $opts = new OptionCollection;
+function opts_init($argv)
+{
+    $opts = new OptionCollection();
 
     $opts->add("h|help", "Print help and exit");
 
     $opts->add("V|version-zmq", "Display ZeroMQ version and exit");
 
-    $opts->add("r|router-endpoint:",
-        "Specify the rounter endpoint address")
+    $opts
+        ->add(
+            "r|router-endpoint:",
+            "Specify the rounter endpoint address"
+        )
         ->isa("string")
-        ->defaultValue("tcp://localhost:5514")
-        ;
+        ->defaultValue("tcp://localhost:5514");
 
-    $opts->add("t|connect-timeout:",
-        "Specify maximum time in seconds for the connection allowed to take")
+    $opts
+        ->add(
+            "t|connect-timeout:",
+            "Specify maximum time in seconds for the connection allowed to take"
+        )
         ->isa("number")
-        ->defaultValue(30)
-        ;
+        ->defaultValue(30);
 
-    $opts->add("q|be-terse",
-        "Be brief and to the point; dump interesting details only");
+    $opts->add(
+        "q|be-terse",
+        "Be brief and to the point; dump interesting details only"
+    );
 
     $parser = new OptionParser($opts);
     try {
         $result = $parser->parse($argv);
         // --help
         if ($result->help) {
-            $printer = new ConsoleOptionPrinter;
+            $printer = new ConsoleOptionPrinter();
             echo "Usage:\n";
             echo "    demo [options] <uri> ...\n\n";
             echo $printer->render($opts);
-            die;
-        // --version-zmq
-        } else if ($result->versionZmq) {
-            echo "ZeroMQ version " . ZMQ::LIBZMQ_VERSION . " ("
-                . ZMQ::LIBZMQ_VERSION_ID . ")\n";
-            die;
+            die();
+            // --version-zmq
+        } elseif ($result->versionZmq) {
+            echo "ZeroMQ version " .
+                ZMQ::LIBZMQ_VERSION .
+                " (" .
+                ZMQ::LIBZMQ_VERSION_ID .
+                ")\n";
+            die();
         }
         $args = $result->getArguments();
     } catch (Exception $e) {
         echo $e->getMessage();
-        die;
+        die();
     }
     return $result;
 }
