f009e9e343d4305361feada78f0aef67a24c8b23
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2020-01-24 20:19:29 +0200

    python3/demo: make changes suggested by black

--- python3/demo/__main__.py
+++ python3/demo/__main__.py
@@ -12,18 +12,21 @@
 
 if __name__ == "__main__":
     from sys import path as sys_path
+
     sys_path.append(".")
 
     try:
         from demo.app import main
+
         main()
     except ModuleNotFoundError as error:
         msg = str(error)
         if "jomiel.protobuf" in msg:
             from sys import stderr, exit
-            notice = 'Did you run the bootstrap script? See README.md'
-            print('error: %s' % msg, file=stderr)
-            print('error: %s' % notice, file=stderr)
+
+            notice = "Did you run the bootstrap script? See README.md"
+            print("error: %s" % msg, file=stderr)
+            print("error: %s" % notice, file=stderr)
             exit(1)
         else:
             raise error
