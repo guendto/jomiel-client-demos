e4f993d7b33f356787a3ba606ab9833aec6a75ac
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2020-01-24 19:56:31 +0200

    python3/__main__: trap ModuleNotFoundError
    Give the user a more meaningful notice.

--- python3/demo/__main__.py
+++ python3/demo/__main__.py
@@ -14,7 +14,18 @@ if __name__ == "__main__":
     from sys import path as sys_path
     sys_path.append(".")
 
-    from demo.app import main
-    main()
+    try:
+        from demo.app import main
+        main()
+    except ModuleNotFoundError as error:
+        msg = str(error)
+        if "jomiel.protobuf" in msg:
+            from sys import stderr, exit
+            notice = 'Did you run the bootstrap script? See README.md'
+            print('error: %s' % msg, file=stderr)
+            print('error: %s' % notice, file=stderr)
+            exit(1)
+        else:
+            raise error
 
 # vim: set ts=4 sw=4 tw=72 expandtab:
