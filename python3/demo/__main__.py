735964338391f91af636b46a174b744fb750def1
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2020-01-24 19:23:13 +0200

    python3/__main__.py: rewrite replacing __init__.py

--- python3/demo/__main__.py
+++ python3/demo/__main__.py
@@ -3,19 +3,18 @@
 # jomiel-examples
 #
 # Copyright
-#  2019 Toni Gündoğdu
+#  2019-2020 Toni Gündoğdu
 #
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 """TODO."""
 
-if __name__ == '__main__':
-    from sys import path
-    path.insert(0, '.')
-    path.insert(1, 'demo/proto/')
+if __name__ == "__main__":
+    from sys import path as sys_path
+    sys_path.append(".")
 
-    from demo import main
+    from demo.app import main
     main()
 
 # vim: set ts=4 sw=4 tw=72 expandtab:
