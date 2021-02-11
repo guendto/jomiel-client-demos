da316af3adf295f28d84d0aa606f038ad1c3dab8
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-02-11 13:35:50 +0200

    style(main.go): replace whitespace with tabspace (go-fmt)

--- golang/main.go
+++ golang/main.go
@@ -15,7 +15,7 @@ package main
 import "github.com/guendto/jomiel-examples/golang/internal/app"
 
 func main() {
-    app.Main()
+	app.Main()
 }
 
 //go:generate ../proto/bin/bootstrap -p ../proto -l go -d internal/gen
