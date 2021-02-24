e00d04e6fab067e37cba83198a64a41b503f5dc7
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-02-24 16:05:40 +0200

    style(jomiel.h): reformat accoding to settings in .clang-format

--- c/src/demo/jomiel.h
+++ c/src/demo/jomiel.h
@@ -18,17 +18,17 @@
 #include "demo/options.h"
 
 struct jomiel_s {
-  options_t* opts;
-  zsock_t* sck;
+  options_t *opts;
+  zsock_t *sck;
 };
 
 typedef struct jomiel_s jomiel_t;
 
-jomiel_t* jomiel_new(options_t*);
-void jomiel_destroy(jomiel_t**);
+jomiel_t *jomiel_new(options_t *);
+void jomiel_destroy(jomiel_t **);
 
-int jomiel_inquire(const jomiel_t*, char*);
+int jomiel_inquire(const jomiel_t *, char *);
 
-#endif  // DEMO_JOMIEL_H
+#endif // DEMO_JOMIEL_H
 
 // vim: set ts=2 sw=2 tw=72 expandtab:
