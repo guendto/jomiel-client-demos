6cfa12584fe14268e8323b8a0676c5dc10853831
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-02-24 16:24:44 +0200

    chore(c/jomiel.h): replace header guards with "#pragma once"

--- c/src/demo/jomiel.h
+++ c/src/demo/jomiel.h
@@ -10,11 +10,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef DEMO_JOMIEL_H
-#define DEMO_JOMIEL_H
+#pragma once
 
 #include <czmq.h>
-
 #include "demo/options.h"
 
 struct jomiel_s {
@@ -28,7 +26,5 @@ jomiel_t *jomiel_new(options_t *);
 void jomiel_destroy(jomiel_t **);
 
 int jomiel_inquire(const jomiel_t *, char *);
-
-#endif // DEMO_JOMIEL_H
 
 // vim: set ts=2 sw=2 tw=72 expandtab:
