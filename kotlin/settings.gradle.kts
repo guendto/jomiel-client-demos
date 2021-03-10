827045f01d7b9790d81950d91fd9fbb78e93a35e
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-10 14:27:42 +0200

    kotlin/settings.gradle.kts: add file

---
+++ kotlin/settings.gradle.kts
@@ -0,0 +1,13 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2021 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+rootProject.name = "jomiel.examples"
