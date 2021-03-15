96dd9983db0f16798c9425053e206277b8fe39b5
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-15 09:46:52 +0200

    scala/build.gradle.kts: add file

---
+++ scala/build.gradle.kts
@@ -0,0 +1,72 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2021 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import com.google.protobuf.gradle.*
+
+plugins {
+    id("com.google.protobuf") version "0.8.15"
+    scala
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("jomiel.examples.App")
+    applicationName = "demo"
+}
+
+dependencies {
+    implementation("com.thesamet.scalapb:scalapb-runtime_2.12:0.10.10")
+    implementation("com.thesamet.scalapb:scalapb-json4s_2.12:0.10.3")
+    implementation("org.tinylog:tinylog-api-scala_2.12:2.2.1")
+    implementation("org.tinylog:tinylog-impl:2.2.1")
+    implementation("info.picocli:picocli:4.6.1")
+    implementation("org.zeromq:jeromq:0.5.2")
+}
+
+sourceSets {
+    main {
+        withConvention(ScalaSourceSet::class) {
+            scala.setSrcDirs(listOf(
+                "src/main/scala",
+                "build/generated/source/proto/main/scalapb"
+            ))
+        }
+        proto {
+            srcDirs("../proto")
+        }
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.15.6"
+    }
+    plugins {
+        id("scalapb") {
+            artifact = "com.thesamet.scalapb:protoc-gen-scala:0.10.10:unix@sh"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach { task ->
+            task.builtins {
+                remove("java")
+            }
+            task.plugins {
+                id("scalapb")
+            }
+        }
+    }
+}
