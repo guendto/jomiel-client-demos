9ae3d1bdf18cbe8d48433e0cbdfd5b29ee068868
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-16 14:05:29 +0200

    refactor/style(Jomiel.java): invert if-statements, reformat
    - Try to improve code readability with
      - Inverted if-statements
      - Separated arg lines
      - Removed braces
    
    - Reformat with intellij
    
    - Remove @SuppressWarnings for PMD

--- java/src/main/java/jomiel/examples/Jomiel.java
+++ java/src/main/java/jomiel/examples/Jomiel.java
@@ -12,14 +12,6 @@
 
 package jomiel.examples;
 
-import static com.google.protobuf.util.JsonFormat.printer;
-import static java.lang.String.format;
-import static java.lang.System.exit;
-import static java.util.Collections.unmodifiableList;
-import static jomiel.protobuf.v1beta1.StatusCode.STATUS_CODE_OK;
-import static org.tinylog.Logger.error;
-import static org.tinylog.Logger.info;
-
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Message;
 import com.google.protobuf.util.JsonFormat.Printer;
@@ -32,109 +24,105 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ.Poller;
 import org.zeromq.ZMQ.Socket;
 
-@SuppressWarnings({
-  "PMD.BeanMembersShouldSerialize",
-  "PMD.DataflowAnomalyAnalysis",
-  "PMD.DoNotCallSystemExit"
-})
+import static com.google.protobuf.util.JsonFormat.printer;
+import static java.lang.String.format;
+import static java.lang.System.exit;
+import static java.util.Collections.unmodifiableList;
+import static jomiel.protobuf.v1beta1.StatusCode.STATUS_CODE_OK;
+import static org.tinylog.Logger.error;
+import static org.tinylog.Logger.info;
+
 final class Jomiel {
-  private final Options opts;
-  private final ZContext ctx = new ZContext();
-  private final Socket sck = ctx.createSocket(SocketType.REQ);
-  private final Poller poller = ctx.createPoller(1);
-  private Printer jsonFormatter = printer();
+    private final Options opts;
+    private final ZContext ctx = new ZContext();
+    private final Socket sck = ctx.createSocket(SocketType.REQ);
+    private final Poller poller = ctx.createPoller(1);
+    private Printer jsonFormatter = printer();
 
-  Jomiel(final Options options) {
-    opts = options;
-    sck.setLinger(0);
-    poller.register(sck, Poller.POLLIN);
-    if (opts.compactJson) {
-      jsonFormatter = printer().omittingInsignificantWhitespace();
+    Jomiel(final Options options) {
+        opts = options;
+        sck.setLinger(0);
+        poller.register(sck, Poller.POLLIN);
+        if (opts.compactJson)
+            jsonFormatter = printer().omittingInsignificantWhitespace();
     }
-  }
 
-  void run() throws InvalidProtocolBufferException {
-    if (opts.uri.isEmpty()) {
-      error("error: input URI not given");
-      exit(1);
+    void run() throws InvalidProtocolBufferException {
+        if (!opts.uri.isEmpty()) {
+            connect();
+            for (final var uri : unmodifiableList(opts.uri))
+                inquire(uri);
+        } else {
+            error("error: input URI not given");
+            exit(1);
+        }
     }
-    connect();
-    for (final var uri : unmodifiableList(opts.uri)) {
-      inquire(uri);
+
+    private void connect() {
+        printStatus(format("<connect> %s (timeout=%d)",
+                opts.routerEndpoint,
+                opts.connectTimeout));
+        sck.connect(opts.routerEndpoint);
     }
-  }
 
-  private void connect() {
-    final var re = opts.routerEndpoint;
-    final var to = opts.connectTimeout;
-    printStatus(format("<connect> %s (timeout=%d)", re, to));
-    sck.connect(re);
-  }
-
-  private void inquire(final String uri) throws InvalidProtocolBufferException {
-    sendInquiry(uri);
-    receiveResponse();
-  }
-
-  private void sendInquiry(final String uri) throws InvalidProtocolBufferException {
-    final var mnq = MediaInquiry.newBuilder().setInputUri(uri).build();
-    final var msg = Inquiry.newBuilder().setMedia(mnq).build();
-    if (!opts.beTerse) {
-      printMessage("<send>", msg);
+    private void inquire(final String uri) throws InvalidProtocolBufferException {
+        sendInquiry(uri);
+        receiveResponse();
     }
-    sck.send(msg.toByteArray());
-  }
 
-  private void receiveResponse() throws InvalidProtocolBufferException {
-    poller.poll(opts.connectTimeout * 1_000);
-    if (poller.pollin(0)) {
-      final var bytes = sck.recv();
-      final var msg = Response.parseFrom(bytes);
-      dumpResponse(msg);
-    } else {
-      error("error: connection timed out");
-      exit(1);
+    private void sendInquiry(final String uri) throws InvalidProtocolBufferException {
+        final var mnq = MediaInquiry.newBuilder().setInputUri(uri).build();
+        final var msg = Inquiry.newBuilder().setMedia(mnq).build();
+        if (!opts.beTerse) printMessage("<send>", msg);
+        sck.send(msg.toByteArray());
     }
-  }
 
-  private void printStatus(final String status) {
-    if (!opts.beTerse) {
-      error("status: " + status);
+    private void receiveResponse() throws InvalidProtocolBufferException {
+        poller.poll(opts.connectTimeout * 1_000);
+        if (poller.pollin(0)) {
+            final var bytes = sck.recv();
+            final var msg = Response.parseFrom(bytes);
+            dumpResponse(msg);
+        } else {
+            error("error: connection timed out");
+            exit(1);
+        }
     }
-  }
 
-  private void dumpResponse(final Response msg) throws InvalidProtocolBufferException {
-    final var media = msg.getMedia();
-    final var status = "<recv>";
-    if (msg.getStatus().getCode() == STATUS_CODE_OK) {
-      if (opts.beTerse) {
-        dumpTerseResponse(media);
-      } else {
-        printMessage(status, media);
-      }
-    } else {
-      printMessage(status, msg);
+    private void printStatus(final String status) {
+        if (!opts.beTerse) error("status: " + status);
     }
-  }
 
-  private void dumpTerseResponse(final MediaResponse msg) {
-    info(format("---%ntitle: %s%nquality:", msg.getTitle()));
-    for (final var stream : unmodifiableList(msg.getStreamList())) {
-      final var quality = stream.getQuality();
-      final String str;
-      str =
-          format(
-              "  profile: %s%n    width: %d%n    height: %d",
-              quality.getProfile(), quality.getWidth(), quality.getHeight());
-      info(str);
+    private void dumpResponse(final Response msg) throws InvalidProtocolBufferException {
+        final var media = msg.getMedia();
+        final var status = "<recv>";
+        if (msg.getStatus().getCode() == STATUS_CODE_OK) {
+            if (opts.beTerse) dumpTerseResponse(media);
+            else printMessage(status, media);
+        } else printMessage(status, msg);
     }
-  }
 
-  private void printMessage(final String status, final Message msg)
-      throws InvalidProtocolBufferException {
-    final String result;
-    result = (opts.outputJson) ? jsonFormatter.print(msg) : msg.toString();
-    printStatus(status);
-    info(result);
-  }
+    private void dumpTerseResponse(final MediaResponse msg) {
+        info(format("---%ntitle: %s%nquality:", msg.getTitle()));
+        for (final var stream : unmodifiableList(msg.getStreamList())) {
+            final var quality = stream.getQuality();
+            final String str;
+            str = format("  profile: %s%n    width: %d%n    height: %d",
+                    quality.getProfile(),
+                    quality.getWidth(),
+                    quality.getHeight());
+            info(str);
+        }
+    }
+
+    private void printMessage(final String status,
+                              final Message msg)
+            throws InvalidProtocolBufferException {
+        final var result =
+                (opts.outputJson)
+                        ? jsonFormatter.print(msg)
+                        : msg.toString();
+        printStatus(status);
+        info(result);
+    }
 }
