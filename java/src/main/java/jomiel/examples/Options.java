a13223ff4f47cb071a3b78140c1c40f1145aab12
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-16 14:15:06 +0200

    refactor/style(Options.java): reformat with intellij
    - Try to improve code readability with
      - Separated arg lines
      - Removed braces
    
    - Reformat with intellij
    
    - Remove @SuppressWarnings for PMD

--- java/src/main/java/jomiel/examples/Options.java
+++ java/src/main/java/jomiel/examples/Options.java
@@ -12,6 +12,14 @@
 
 package jomiel.examples;
 
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
 import static java.lang.String.format;
 import static java.lang.System.exit;
 import static org.tinylog.Logger.info;
@@ -19,90 +27,78 @@ import static org.zeromq.ZMQ.getFullVersion;
 import static org.zeromq.ZMQ.getVersionString;
 import static picocli.CommandLine.usage;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-
-@SuppressWarnings({"PMD.BeanMembersShouldSerialize", "PMD.DoNotCallSystemExit"})
+@SuppressWarnings({"CanBeFinal", "FieldMayBeFinal"})
 @Command(name = "demo", usageHelpAutoWidth = true)
 final class Options implements Callable<Integer> {
 
-  @Option(
-      names = {"-h", "--help"},
-      usageHelp = true,
-      description = "Display this help message and exit")
-  private boolean help = false;
+    @Option(names = {"-h", "--help"},
+            usageHelp = true,
+            description = "Display this help message and exit")
+    private boolean help = false;
 
-  @Option(
-      names = {"-D", "--print-config"},
-      description = "Print configuration values and exit")
-  private boolean printConfig = false;
+    @Option(names = {"-D", "--print-config"},
+            description = "Print configuration values and exit")
+    private boolean printConfig = false;
 
-  @Option(
-      names = {"-r", "--router-endpoint"},
-      paramLabel = "<addr>",
-      description = "Specify the router endpoint address (${DEFAULT-VALUE})")
-  String routerEndpoint = "tcp://localhost:5514";
+    @Option(names = {"-r", "--router-endpoint"},
+            paramLabel = "<addr>",
+            description = "Specify the router endpoint address "
+                    + "(${DEFAULT-VALUE})")
+    String routerEndpoint = "tcp://localhost:5514";
 
-  @Option(
-      names = {"-t", "--connect-timeout"},
-      paramLabel = "<timeout>",
-      description =
-          "Specify maximum time in seconds for the connection "
-              + "allowed to take (${DEFAULT-VALUE})")
-  long connectTimeout = 30;
+    @Option(names = {"-t", "--connect-timeout"},
+            paramLabel = "<timeout>",
+            description = "Specify maximum time in seconds for the "
+                    + "connection allowed to take "
+                    + "(${DEFAULT-VALUE})")
+    long connectTimeout = 30;
 
-  @Option(
-      names = {"-V", "--version-zmq"},
-      description = "Display ZeroMQ version and exit")
-  private boolean zmqVersion = false;
+    @Option(names = {"-V", "--version-zmq"},
+            description = "Display ZeroMQ version and exit")
+    private boolean zmqVersion = false;
 
-  @Option(
-      names = {"-j", "--output-json"},
-      description = "Print dumped messages in JSON")
-  boolean outputJson;
+    @Option(names = {"-j", "--output-json"},
+            description = "Print dumped messages in JSON")
+    boolean outputJson = false;
 
-  @Option(
-      names = {"-c", "--compact-json"},
-      description = "Use more compact representation of JSON")
-  boolean compactJson;
+    @Option(names = {"-c", "--compact-json"},
+            description = "Use more compact representation of JSON")
+    boolean compactJson = false;
 
-  @Option(
-      names = {"-q", "--be-terse"},
-      description = "Be brief and to the point; dump interesting details only")
-  boolean beTerse;
+    @Option(names = {"-q", "--be-terse"},
+            description = "Be brief and to the point; dump interesting "
+                    + "details only")
+    boolean beTerse = false;
 
-  @Parameters(arity = "0..*", paramLabel = "<uri>", description = "the URIs to parse")
-  List<String> uri = new ArrayList<>();
+    @Parameters(arity = "0..*",
+            paramLabel = "<uri>",
+            description = "the URIs to parse")
+    List<String> uri = new ArrayList<>();
 
-  @Override
-  public final Integer call() throws Exception {
-    if (help) {
-      usage(this, System.out);
-    } else if (printConfig) {
-      dumpConfig();
-    } else if (zmqVersion) {
-      printZmqVersion();
-    } else {
-      new Jomiel(this).run();
+    @Override
+    public final Integer call() throws Exception {
+        if (help) usage(this, System.out);
+        else if (printConfig) dumpConfig();
+        else if (zmqVersion) printZmqVersion();
+        else new Jomiel(this).run();
+        return 0;
     }
-    return 0;
-  }
 
-  private void dumpConfig() throws IllegalAccessException {
-    info("---");
-    for (final var field : getClass().getDeclaredFields()) {
-      field.setAccessible(true);
-      info(format("%s: %s", field.getName(), field.get(this)));
+    private void dumpConfig() throws IllegalAccessException {
+        info("---");
+        for (final var field : getClass().getDeclaredFields()) {
+            field.setAccessible(true);
+            info(format("%s: %s",
+                    field.getName(),
+                    field.get(this)));
+        }
+        exit(0);
     }
-    exit(0);
-  }
 
-  private void printZmqVersion() {
-    info(format("ZeroMQ version %s (%s)", getVersionString(), getFullVersion()));
-    exit(0);
-  }
+    private void printZmqVersion() {
+        info(format("ZeroMQ version %s (%s)",
+                getVersionString(),
+                getFullVersion()));
+        exit(0);
+    }
 }
