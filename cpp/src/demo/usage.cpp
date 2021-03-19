e912db079c72d494dd4afb33e6ae1d85cb31cf1c
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-19 16:54:47 +0200

    refactor(usage.cpp): re-organize display order

--- cpp/src/demo/usage.cpp
+++ cpp/src/demo/usage.cpp
@@ -12,22 +12,22 @@
 
 const char *usage =
     R"(Usage:
-    demo [-hDVqjc] [-r <addr>] [-t <time>] [URI ...]
+    demo [-hDVjcq] [-r <addr>] [-t <time>] [URI ...]
 
 Options:
     -h --help                       Print this help and exit
     -D --print-config               Print configuration values and exit
-    -V --version-zmq                Print ZeroMQ version and exit
     -r --router-endpoint <addr>     Specify the router endpoint address
                                      [default: tcp://localhost:5514]
     -t --connect-timeout <time>     Specify maximum time in seconds for
                                      the connection allowed to take
                                      [default: 30]
-    -q --be-terse                   Be brief and to the point; dump
-                                     interesting details only
+    -V --version-zmq                Print ZeroMQ version and exit
     -j --output-json                Print dumped messages in JSON
     -c --compact-json               Use more compact representation of
                                      JSON
+    -q --be-terse                   Be brief and to the point; dump
+                                     interesting details only
 
 )";
 
