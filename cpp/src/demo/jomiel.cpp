b7438486aece99d73de4549a1189fe7bfb93a0ac
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-22 16:07:46 +0200

    refactor(jomiel.cpp): remove dtor

--- cpp/src/demo/jomiel.cpp
+++ cpp/src/demo/jomiel.cpp
@@ -131,22 +131,6 @@ void jomiel::to_json(gp::Message const &msg, std::string &dst) const {
     dst += "\n";
 }
 
-void jomiel::cleanup() const {
-  /*
-   * "Also notice the call to ShutdownProtobufLibrary() at the end of
-   * the program. All this does is delete any global objects that were
-   * allocated by the Protocol Buffer library. This is unnecessary for
-   * most programs, since the process is just going to exit anyway and
-   * the OS will take care of reclaiming all of its memory. However, if
-   * you use a memory leak checker that requires that every last object
-   * be freed, or if you are writing a library which may be loaded and
-   * unloaded multiple times by a single process, then you may want to
-   * force Protocol Buffers to clean up everything."
-   *  <https://developers.google.com/protocol-buffers/docs/cpptutorial>
-   */
-  gp::ShutdownProtobufLibrary();
-}
-
 // cppzmq compatibility functions.
 //
 
