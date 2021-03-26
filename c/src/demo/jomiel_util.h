e568fbfe3f598831b4309becc0c8fef1b516af11
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-26 18:54:23 +0200

    refactor(c/jomiel_util.h): add file

---
+++ c/src/demo/jomiel_util.h
@@ -0,0 +1,33 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2021 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "demo/options.h"
+
+#include <czmq.h>
+
+// Structures.
+
+struct Jomiel {
+  Options opts;
+  zsock_t *sck;
+  bool result;
+};
+
+typedef struct Jomiel Jomiel;
+
+// Functions.
+
+Jomiel jutil_connect(Jomiel);
+Jomiel jutil_send_inquiry(Jomiel);
+Jomiel jutil_receive_response(Jomiel);
