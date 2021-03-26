42b29016f4d48fde4da8ceaec00b87a312cecfc1
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-26 19:30:47 +0200

    refactor(c/jomiel.h): remove redundant declarations

--- c/src/demo/jomiel.h
+++ c/src/demo/jomiel.h
@@ -17,12 +17,3 @@
 // Functions.
 
 int jomiel_run(Options const);
-
-typedef struct jomiel_s jomiel_t;
-
-jomiel_t *jomiel_new(options_t *);
-void jomiel_destroy(jomiel_t **);
-
-int jomiel_inquire(const jomiel_t *, char *);
-
-// vim: set ts=2 sw=2 tw=72 expandtab:
