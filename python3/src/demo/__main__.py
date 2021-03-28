be5ffcf1d6a096ff5de741a094a3c84402f5bf1f
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-03-28 14:56:59 +0300

    refactor(python3/__main__.py): add file in /src/demo

---
+++ python3/src/demo/__main__.py
@@ -0,0 +1,19 @@
+#
+# jomiel
+#
+# Copyright
+#  2021 Toni Gündoğdu
+#
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+def main():
+    from demo.runner import Runner
+
+    Runner()
+
+
+if __name__ == "__main__":
+    main()
