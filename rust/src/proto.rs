24487cb68b18033ef52760b7966cc68a0e19f421
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-04-04 18:23:55 +0300

    chore(proto.rs): add file

---
+++ rust/src/proto.rs
@@ -0,0 +1,15 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2021 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+pub mod media;
+pub mod message;
+pub mod status;
