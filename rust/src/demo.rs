92e2a4db81ae35a12aeb9bf2833dd774464e3d76
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-04-04 18:18:10 +0300

    chore(demo.rs): add file

---
+++ rust/src/demo.rs
@@ -0,0 +1,15 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2021 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+pub mod jomiel;
+pub mod options;
+pub mod runner;
