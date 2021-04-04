a740fcd45015dcb580c5826c657009aa27ae1b3f
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-04-04 18:19:19 +0300

    chore(demo/runner.rs): add file

---
+++ rust/src/demo/runner.rs
@@ -0,0 +1,36 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-examples
+ *
+ * Copyright
+ *  2021 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+use super::jomiel::Jomiel;
+use super::options::Options;
+use structopt::StructOpt;
+
+pub fn main() {
+    let opts = Options::from_args();
+    if opts.print_config {
+        dump_config(&opts);
+    } else if opts.version_zmq {
+        print_zmq_version();
+    } else {
+        Jomiel::new(opts).inquire();
+    }
+}
+
+fn dump_config(opts: &Options) {
+    let s = serde_yaml::to_string(opts).unwrap();
+    print!("{}", s);
+}
+
+fn print_zmq_version() {
+    let (major, minor, patch) = zmq::version();
+    println!("ZeroMQ version {}.{}.{}", major, minor, patch);
+}
