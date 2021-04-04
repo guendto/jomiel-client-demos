7bf9b03248326b0c346334c2e27c933139d19d9d
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-04-04 18:16:53 +0300

    refactor(main.rs): rewrite for demo::runner::main()

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,56 +4,17 @@
  * jomiel-examples
  *
  * Copyright
- *  2019 Toni Gündoğdu
+ *  2019-2021 Toni Gündoğdu
  *
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-extern crate protobuf;
-extern crate structopt;
-extern crate zmq;
-
-/*
-extern crate serde;
-extern crate serde_derive;
-extern crate serde_json;
-*/
-
-use jomiel::Jomiel;
-
-mod jomiel;
-mod options;
+mod demo;
 mod proto;
 
+use demo::runner;
+
 fn main() {
-    let jomiel = Jomiel::new();
-
-    if jomiel.opts.print_config {
-        print_config(&jomiel);
-    } else if jomiel.opts.version_zmq {
-        print_zmq_version()
-    }
-
-    if jomiel.opts.uri.is_empty() {
-        println!("error: input URI not given");
-        std::process::exit(1);
-    }
-
-    jomiel.connect();
-
-    for uri in &jomiel.opts.uri {
-        jomiel.inquire(uri);
-    }
-}
-
-fn print_config(jomiel: &Jomiel) {
-    println!("{:#?}", jomiel.opts);
-    std::process::exit(0);
-}
-
-fn print_zmq_version() {
-    let (major, minor, patch) = zmq::version();
-    println!("ZeroMQ version {}.{}.{}", major, minor, patch);
-    std::process::exit(0);
+    runner::main();
 }
