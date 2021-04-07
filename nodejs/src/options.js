ddad7b02ff7e6c3e59118ae76903318ae2548230
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-04-07 20:01:52 +0300

    refactor(options.js): rewrite for docopt

--- nodejs/src/options.js
+++ nodejs/src/options.js
@@ -4,37 +4,33 @@
  * jomiel-examples
  *
  * Copyright
- *  2020 Toni Gündoğdu
+ *  2021 Toni Gündoğdu
  *
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-"use strict";
+const USAGE = `
+Usage:
+    demo [-hDVjcq] [-r <addr>] [-t <time>] [URI ...]
+
+Options:
+     -h --help                      Print this help and exit
+     -D --print-config              Print configuration values and exit
+     -V --version-zmq               Print ZeroMQ version and exit
+     -j --output-json               Print dumped messages in JSON
+     -c --compact-json              Use more compact representation of JSON
+     -r --router-endpoint <addr>    Specify the router endpoint address
+                                     [default: tcp://localhost:5514]
+     -t --connect-timeout <time>    Specify maximum time in seconds for
+                                     the connection allowed to take
+                                     [default: 30]
+     -q --be-terse                  Be brief and to the point; dump
+                                     interesting details only
+`;
 
 const parse = () => {
-  const cmdr = require("commander");
-
-  cmdr
-    .usage("[options] <uri ...>")
-    .option(
-      "-r, --router-endpoint <addr>",
-      "Specify the router endpoint address",
-      "tcp://localhost:5514"
-    )
-    .option(
-      "-t, --connect-timeout <secs>",
-      "Specify maximum time in seconds for the connection allowed to take",
-      30
-    )
-    .option("-V, --version-zmq", "Display ZeroMQ version and exit")
-    .option("-j, --output-json", "Print dumped messages in JSON")
-    .option(
-      "-q, --be-terse",
-      "Be brief and to the point; dump interesting details only"
-    );
-
-  return cmdr.parse(process.argv);
+  return require("docopt").docopt(USAGE);
 };
 
 module.exports = { parse };
