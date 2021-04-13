d08f55e678e49d0750965c0aa3a3354d1ed8b40a
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-04-13 16:02:11 +0300

    build(php/getrtdeps.sh): install php extensions conditionally
    Install (for alpine v3.13):
    - php7-pecl-zmq if php7
    - php8-pecl-zmq from alpine `edge/testing` if php8

--- php/docker/bin/getrtdeps.sh
+++ php/docker/bin/getrtdeps.sh
@@ -10,6 +10,22 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-apk --no-cache add \
-    protobuf \
-    zeromq
+
+# $1 = php7 or php8
+PHP8_REPO=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
+PHP_EXT="$1-sodium"
+
+# alpine 3.13 does not package php8-pecl-zmq, it's in testing.
+# - install php7-pecl-zmq ony, for php8-pecl-zmq, see further down
+[ $1 = "php7" ] && PHP_EXT="$PHP_EXT $1-pecl-zmq"
+
+apk --no-cache add $PHP_EXT protobuf zeromq
+
+# install php8-pecl-zmq from edge/testing.
+if [ $? -eq 0 ]; then
+    if [ $1 = "php8" ]; then
+        apk --no-cache add php8-pecl-zmq --repository=$PHP8_REPO
+    fi
+fi
+
+exit $?
