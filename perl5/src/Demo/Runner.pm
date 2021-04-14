6a6969f000312125535712e1452243beb0f17c1b
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-04-14 13:43:57 +0300

    style(Runner.pm): move "use strict" (and friends) at the top (+FATAL=>all)

--- perl5/src/Demo/Runner.pm
+++ perl5/src/Demo/Runner.pm
@@ -9,11 +9,12 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-package Demo::Runner {
+use strict;
+use warnings;
+use warnings FATAL => 'all';
+use v5.20;
 
-  use warnings;
-  use strict;
-  use v5.20;
+package Demo::Runner {
 
   use feature 'signatures';
   no warnings 'experimental::signatures';
