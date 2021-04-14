8e192adc82ef30767ddc29a63c6c6f6af8084f8f
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-04-14 11:49:45 +0300

    style(options.lua): use PascalCase for class name

--- lua5/src/demo/options.lua
+++ lua5/src/demo/options.lua
@@ -13,39 +13,39 @@
 local Options = {}
 
 function Options.parse()
-  local parser = require 'argparse' 'demo'
+  local Parser = require 'argparse' 'demo'
 
-  parser:flag('-D --print-config',
+  Parser:flag('-D --print-config',
     'Print confuguration values and exit')
 
-  parser:flag('-V --version-zmq',
+  Parser:flag('-V --version-zmq',
     'Display ZeroMQ version and exit', false)
 
-  parser:flag('-j --output-json',
+  Parser:flag('-j --output-json',
     'Print dumped messages in JSON', false)
 
-  parser:flag('-c --compact-json',
+  Parser:flag('-c --compact-json',
     'Use more compact representation of JSON', false)
 
-  parser:option('-r --router-endpoint',
+  Parser:option('-r --router-endpoint',
     'Specify the outer endpoint address',
     'tcp://localhost:5514')
 
-  parser:option('-t --connect-timeout',
+  Parser:option('-t --connect-timeout',
     'Specify maximum time in seconds for the connection allowed to take',
     30)
 
-  parser:flag('-q --be-terse',
+  Parser:flag('-q --be-terse',
     'Be brief and to the point; dump interesting details only', false)
 
-  parser:option('-p --proto-dir',
+  Parser:option('-p --proto-dir',
     'Specify path to the dir with the .proto files',
     '../proto'
   )
 
-  parser:argument('URI'):args('*')
+  Parser:argument('URI'):args('*')
 
-  return parser:parse()
+  return Parser:parse()
 end
 
 return Options
