dd8b657f5eb0be16c0de6797d50a617895fb99ba
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-05-08 14:29:54 +0300

    refactor(express/handler.js): require jomiel-messages

--- rest-api/express/src/handler.js
+++ rest-api/express/src/handler.js
@@ -10,7 +10,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const { StatusCode } = require("./messages").jomiel.protobuf.v1beta1;
+const {
+  StatusCode
+} = require("jomiel-messages").jomiel.protobuf.v1beta1;
 const { Jomiel } = require("./jomiel");
 
 class InquiryHandler {
