889b462e5e89484296b9fb0f1b11ccd55f374626
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-05-09 16:44:39 +0300

    refactor(express/index.js): exit process gracefully

--- rest-api/express/src/index.js
+++ rest-api/express/src/index.js
@@ -32,11 +32,27 @@ app.use((error, req, res, next) => {
   return res.status(500).json({ status: error.toString() });
 });
 
-app.listen(process.env.PORT || 3001, () => {
+const server = app.listen(process.env.PORT || 3001, () => {
   console.log(`<listen> at http://localhost:${process.env.PORT}`);
   inquiryRouter.stack.forEach(layer => {
     const route = layer.route;
     const method = route.stack[0].method;
     console.log(`<endpoint> ${method} /inquiry${route.path}`);
   });
+});
+
+// Exit gracefully.
+
+const cleanUp = () => {
+  server.close(() => {
+    console.log("<exit> process gracefully");
+  });
+};
+
+process.on("SIGTERM", () => {
+  cleanUp();
+});
+
+process.on("SIGINT", () => {
+  process.kill(process.pid, "SIGTERM");
 });
