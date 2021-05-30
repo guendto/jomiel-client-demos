c681f9de899041f9b691c6d9849245927aa4c8a3
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-05-30 17:45:46 +0300

    refactor(express/handler): ESMify

--- rest-api/express/src/handler.js
+++ rest-api/express/src/handler.js
@@ -9,14 +9,13 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-"use strict";
 
-const {
-  StatusCode
-} = require("jomiel-messages").jomiel.protobuf.v1beta1;
-const { Jomiel } = require("./jomiel");
+import jomielMessages from "jomiel-messages";
+import { Jomiel } from "./jomiel.js";
 
-class InquiryHandler {
+const { StatusCode } = jomielMessages.jomiel.protobuf.v1beta1;
+
+export class InquiryHandler {
   #jomiel = new Jomiel();
 
   async handle(req, res) {
@@ -49,5 +48,3 @@ class InquiryHandler {
       : failure(jomielResponse, expressResponse);
   }
 }
-
-module.exports = { InquiryHandler };
