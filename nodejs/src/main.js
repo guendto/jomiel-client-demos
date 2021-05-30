88d4fff445b0fb2155ea53efb85c252473c7f0e3
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-05-30 17:13:46 +0300

    refactor(nodejs/main): merge runner.js into main.js

--- nodejs/src/main.js
+++ nodejs/src/main.js
@@ -9,5 +9,28 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-import { main } from "./runner.js";
-main();
+import { dump as dumpYAML } from "js-yaml";
+import { version } from "zeromq";
+
+import { Jomiel } from "./jomiel.js";
+import { parse } from "./options.js";
+
+const dump_config = opts => {
+  let values = {};
+  Object.entries(opts).forEach(([key, value]) => {
+    key = key.replace("--", "");
+    values[key] = value;
+  });
+  process.stdout.write(`---\n${dumpYAML(values)}`);
+};
+
+(() => {
+  const opts = parse();
+  if (opts["--print-config"]) {
+    dump_config(opts);
+  } else if (opts["--version-zmq"]) {
+    console.log("ZeroMQ version %s", version);
+  } else {
+    new Jomiel(opts).inquire();
+  }
+})();
