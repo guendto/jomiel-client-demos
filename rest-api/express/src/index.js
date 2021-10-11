8419a74132418d31e65ccaa7802c1260e8984f3f
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-10-11 15:20:49 +0300

    refactor(express/index): use IIFE, error middlewares, config module

--- rest-api/express/src/index.js
+++ rest-api/express/src/index.js
@@ -12,46 +12,38 @@
 
 /* eslint-disable no-console */
 
-import { config } from "dotenv";
 import express from "express";
 
-// eslint-disable-next-line import/extensions
+/* eslint-disable import/extensions */
+
 import inquiryRouter from "./controllers/inquiry.js";
+import errorHandler from "./middlewares/error.js";
+import http404 from "./middlewares/http404.js";
+import config from "./config.js";
 
-config();
-const app = express();
+(() => {
+  const app = express();
 
-app.use(express.json());
-app.use("/inquiry", inquiryRouter);
+  app.use(express.json());
+  app.use("/inquiry", inquiryRouter);
+  app.use(errorHandler);
+  app.use(http404);
 
-app.get("*", (req, res, next) => {
-  const error = new Error(
-    `${req.ip} tried to access ${req.originalUrl}`
-  );
-  error.StatusCode = 301;
-  next(error);
-});
-
-app.use((error, req, res, next) =>
-  res.status(500).json({ status: error.toString() })
-);
-
-const server = app.listen(process.env.PORT || 3001, () => {
-  console.log(`<listen> at http://localhost:${process.env.PORT}`);
-  inquiryRouter.stack.forEach((layer) => {
-    const { route } = layer;
-    const { method } = route.stack[0];
-    console.log(`<endpoint> ${method} /inquiry${route.path}`);
+  const server = app.listen(config.PORT, () => {
+    console.log(`<listen> at http://localhost:${config.PORT}`);
+    inquiryRouter.stack.forEach((layer) => {
+      const { route } = layer;
+      const { method } = route.stack[0];
+      console.log(`<endpoint> ${method} /inquiry${route.path}`);
+    });
   });
-});
 
-// Exit gracefully.
+  const cleanUp = () => {
+    server.close(() => {
+      console.log("<exit> process gracefully");
+    });
+  };
 
-const cleanUp = () => {
-  server.close(() => {
-    console.log("<exit> process gracefully");
-  });
-};
-
-process.on("SIGTERM", () => cleanUp());
-process.on("SIGINT", () => process.kill(process.pid, "SIGTERM"));
+  process.on("SIGINT", () => process.kill(process.pid, "SIGTERM"));
+  process.on("SIGTERM", () => cleanUp());
+})();
