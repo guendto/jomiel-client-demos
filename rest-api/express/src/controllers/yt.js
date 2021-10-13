47af2048c4c07504f638e3d4ea2349b2d8f499ed
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2021-10-13 14:32:47 +0300

    chore(express/controllers/yt): add module

---
+++ rest-api/express/src/controllers/yt.js
@@ -0,0 +1,27 @@
+/*
+ * -*- coding: utf-8 -*-
+ *
+ * jomiel-client-demos
+ *
+ * Copyright
+ *  2021 Toni Gündoğdu
+ *
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Router } from "express";
+import "express-async-errors";
+
+/* eslint-disable import/extensions */
+import jomiel from "../helpers/jomiel.js";
+
+const router = Router();
+
+// GET /api/yt/by-id/:id
+router.get("/:id([0-9A-Za-z_-]{11})", async (req, res) => {
+  req.body.inputUri = `https://youtu.be/${req.params.id}`;
+  await jomiel.sendInquiry(res, req.body);
+});
+
+export default router;
