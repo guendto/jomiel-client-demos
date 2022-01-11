3000fddccd1eb3fdb96ed1925f0ecbf83dab298c
Toni Gündoğdu <47444442+guendto@users.noreply.github.com>
2022-01-11 18:58:17 +0200

    build(deps): bump org.tinylog:tinylog-impl 2.2.1 to 2.4.1 in /scala

--- scala/build.gradle.kts
+++ scala/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     implementation("com.thesamet.scalapb:scalapb-runtime_2.12:0.11.8")
     implementation("com.thesamet.scalapb:scalapb-json4s_2.12:0.12.0")
     implementation("org.tinylog:tinylog-api-scala_2.12:2.4.1")
-    implementation("org.tinylog:tinylog-impl:2.2.1")
+    implementation("org.tinylog:tinylog-impl:2.4.1")
     implementation("info.picocli:picocli:4.6.1")
     implementation("org.zeromq:jeromq:0.5.2")
 }
